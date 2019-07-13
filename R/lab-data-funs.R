38005ce71c7940b7d733a8b16ab7d02a58fea634
Michelle Stuart <michelle.stuart@rutgers.edu>
2019-07-13 14:14:11 -0400

    update plate_from_db()

--- R/lab-data-funs.R
+++ R/lab-data-funs.R
@@ -12,16 +12,16 @@
 plate_from_db <- function(table_name, ...){
 
   # split the well out into row and column
-  table_name <- table_name %>%
+  platemap <- table_name %>%
     dplyr::mutate(row = stringr::str_sub(well, 1,1),
                   col = as.numeric(stringr::str_sub(well, 2,3)))
 
   # select columns for plate
-  table_name <- table_name %>%
-    select(row, col, ...) %>% #keep row & col, identifier
-    arrange(row, col)
+  platemap <- platemap %>%
+    dplyr::select(row, col, ...) %>% #keep row & col, identifier
+    dplyr::arrange(row, col)
 
-    # make map
-  platemap <<- as.matrix(reshape2::acast(table_name,table_name[,1] ~ table_name[,2]))
-  return(table_name)
+    # make map and return object platemap as well as the table
+  platemap <- as.matrix(reshape2::acast(platemap,platemap[,1] ~ platemap[,2]))
+  return(platemap)
 }
