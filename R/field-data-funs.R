6aee1fcac23a6880aa5eee876eade1a6e8796795
Michelle Stuart <michelle.stuart@rutgers.edu>
2019-07-13 11:44:47 -0400

    add roxygen skeleton to get_fish()

--- R/field-data-funs.R
+++ R/field-data-funs.R
@@ -1,6 +1,16 @@
+#' Pull the clownfish table from the leyte database
+#'
+#' This function pulls in the entire clownfish table which can be filtered for a more specific query.
+#'
+#' @return A tibble/data frame
+#' @export
+#'
+#' @examples
+#' fish <- get_fish() %>%
+#' filter(sample_id == "APCL18_201")
 get_fish <- function(){
   if(!exists("leyte"))
-    stop("Error: db connection called 'leyte' does not exist, see Michelle for help")
+    stop("ErrorZ: db connection called 'leyte' does not exist, see Michelle for help")
   fish <- leyte %>%
     dplyr::tbl("clownfish") %>%
     dplyr::collect()
